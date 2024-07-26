c147d0bc57edb1b1f4b0bbb40eccbaa4f119f036
“treyp99” <nwachukwupgate99@gmail.com>
2024-07-27 00:30:59 +0100

    new one

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPIcker";
+import AiPicker from "./AiPicker";
+import FilePicker from "./FilePicker";
+import TabPicker from "./Tab";
+
+export { CustomButton, ColorPicker, AiPicker, FilePicker, TabPicker } ;
